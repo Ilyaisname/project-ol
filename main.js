2a4c81934d6b67c97b70bc072f87ce0a52b7966c
Ilya <microstim@mail.ru>
2020-03-17 00:04:07 +0300

    modified file main.js

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
- let arr = ['apples', 'banana', 'orange', 'mandarin', 'purl'];
+ let arr = [
+   {name: 'apples', id:'2020-03-16_23:30'}, 
+   {name: 'banana', id:'2020-03-16_23:31'}, 
+   {name: 'orange', id:'2020-03-16_23:32'}, 
+   {name: 'mandarin', id:'2020-03-16_23:33'},
+   {name: 'purl', id:'2020-03-16_23:34'}
+  ];
 
 function createUl() {
   let ul = document.createElement('ul');
   let li = '';
 
   arr.forEach(element => {
-    li += '<li>' + element + `<button class='but'> remove </button>` + '</li>';
+    li += '<li>' + element.name + `<button class='but'> remove </button>` + '</li>';
   });
 
   ul.innerHTML = li;
   
   document.body.append(ul);
-
+  
   document.querySelectorAll('.but').forEach(elem => {
-    elem.onclick = function() {
+    elem.onclick = function(this) {
       this.parentElement.remove();
-      arr.find((item, index) => {
-       if (item === this.previousSibling.data) {
-         arr.splice(index, 1);
-        }
-      });
+      // arr.map((item, index) => {
+      //  if (item ===  ) {
+      //    arr.splice(index, 1);
+      //   }
+      // });
     }
   });
 }
